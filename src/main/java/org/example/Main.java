8ecd4e73fcbc32176eba8804ce3de709b3b31e89
TimurMukashov <morok278@gmail.com>
2024-09-22 15:00:49 +0500

    test3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Privet");
+        System.out.println("Privet Friend");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
