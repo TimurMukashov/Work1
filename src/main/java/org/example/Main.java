32f343e0626fc09a4bb049bbe7aa7927f6007c2a
TimurMukashov <morok278@gmail.com>
2024-09-23 23:19:08 +0500

    test task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,27 @@
 package org.example;
 
+import javax.annotation.processing.Filer;
+import java.io.*;
+import java.util.*;
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        System.out.println("Privet Friend");
+        PriorityQueue<Integer> queue1 = new PriorityQueue<>();
+        for (int i = 0; i <= 20; i += 2) {
+            queue1.add(i);
+        }
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        PriorityQueue<Integer> queue2 = new PriorityQueue<>();
+        for (int i = 1; i < 20; i += 2) {
+            queue2.add(i);
+        }
+
+        PriorityQueue<Integer> queue3 = new PriorityQueue<>();
+        queue3.addAll(queue1);
+        queue3.addAll(queue2);
+        while (!queue3.isEmpty()) {
+            System.out.println(queue3.poll());
         }
     }
 }
