18165857f723705641c604ea1e93a0e316947b7d
TimurMukashov <morok278@gmail.com>
2024-09-23 23:22:31 +0500

    change code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,25 +3,28 @@ package org.example;
 import javax.annotation.processing.Filer;
 import java.io.*;
 import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        PriorityQueue<Integer> queue1 = new PriorityQueue<>();
+        Queue<Integer> queue1 = new LinkedList<>();
+        Queue<Integer> queue2 = new LinkedList<>();
+        PriorityQueue<Integer> resultQueue = new PriorityQueue<>();
+
         for (int i = 0; i <= 20; i += 2) {
             queue1.add(i);
         }
 
-        PriorityQueue<Integer> queue2 = new PriorityQueue<>();
         for (int i = 1; i < 20; i += 2) {
             queue2.add(i);
         }
 
-        PriorityQueue<Integer> queue3 = new PriorityQueue<>();
-        queue3.addAll(queue1);
-        queue3.addAll(queue2);
-        while (!queue3.isEmpty()) {
-            System.out.println(queue3.poll());
+        resultQueue.addAll(queue1);
+        resultQueue.addAll(queue2);
+
+        while (!resultQueue.isEmpty()) {
+            System.out.print(resultQueue.poll() + " ");
         }
     }
 }
